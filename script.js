5651c49f491d8cbaa99d60b9a7827ec6086dbf5b
pukpuk555 <jester2540@gmail.com>
2024-05-14 15:32:42 +0700

    complete

---
+++ script.js
@@ -0,0 +1,22 @@
+const isEven = function (a, b) {
+  if (a === b) {
+    console.log("It's even");
+  } else {
+    console.log("It's not even");
+  }
+};
+
+a = 4;
+b = 3;
+
+isEven(a, b);
+
+const isEvenTwo = (a, b) => {
+  if (a === b) {
+    console.log("It's even");
+  } else {
+    console.log("It's not even");
+  }
+};
+
+isEvenTwo(a, b);
